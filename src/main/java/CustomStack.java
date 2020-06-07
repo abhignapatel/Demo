9fcd15e30b6de4b80b7752285ecedb20f8f5ed99
Jay Donga <jaydonga@gmail.com>
2020-06-07 22:18:11 +0530

    stack implementation using array

--- src/main/java/CustomStack.java
+++ src/main/java/CustomStack.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class CustomStack {
-    //    private int[] array;
-//    private int i;
-    Node head;
+        private int[] array;
+    private int i;
+//    Node head;
 
 
     CustomStack(int capacity) throws Exception {
@@ -9,37 +11,42 @@ public class CustomStack {
             throw new Exception("capacity should not be zero and less than zero");
 
         }
-//        array = new int[capacity];
-//        i = 0; //is empty
+        array = new int[capacity];
+        i = 0; //is empty
 
     }
 
     public void push(int data) {
         Node temp = new Node(data);
-//        if (i == array.length) {
-//
-//            array = Arrays.copyOf(array, array.length * 2);
-//
-//        }
+        if (i == array.length) {
 
-        temp.next = head;
-        head = temp;
-//        i++;
+            array = Arrays.copyOf(array, array.length * 2);
+
+        }
+
+//        temp.next = head;
+//        head = temp;
+        array[i] = data;
+        i++;
 
 
     }
 
     public int pop(){
-        int data = 0;
-        if (head != null) {
-            data= head.data;
-            head = head.next;
-//            i--;
-//            return array[i];
-            return data;
-
+//        int data = 0;
+//        if (head != null) {
+//            data= head.data;
+//            head = head.next;
+////            i--;
+////            return array[i];
+//            return data;
+//
+//        }
+        if (i>0){
+            i--;
+            return array[i];
         }
         System.out.println("stack is empty");
-        return  data;
+        return  -1;
     }
 }
